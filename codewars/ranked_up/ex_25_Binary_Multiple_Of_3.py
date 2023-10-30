e22d3a3667856470a31c60295d0bc4356bff11c4
Oleksii kozulia <mistosisi@gmail.com>
2023-10-30 16:57:03 +0200

    codewars foundation and rankedup 4kyu upgrade

--- codewars/ranked_up/ex_25_Binary_Multiple_Of_3.py
+++ codewars/ranked_up/ex_25_Binary_Multiple_Of_3.py
@@ -4,6 +4,9 @@ def multiple_of_3(binary_string):
     pattern = r"^(0*(1(01*0)*10*)*)$"
     return re.match(pattern, binary_string) is not None
 
+
+PATTERN = re.compile(r'^(0*(1(01*0)*10*)*)$')
+
 # Example usage:
 print(multiple_of_3('000'))  # True
 print(multiple_of_3('001'))  # False
